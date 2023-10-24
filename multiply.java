f32652ba2c80a11034edcc2fa4e93f846caa9a85
shivang2405 <shivangagarwal2405@gmail.com>
2023-10-25 01:02:59 +0530

    Deleted file multiply.java

--- multiply.java
+++
@@ -1,8 +0,0 @@
-public class multiply{
-    public static void main(String[] args){
-        int a = 5;
-        int b = 10;
-        int c = a * b;
-        System.out.println("The product is " + c);
-    }
-}
