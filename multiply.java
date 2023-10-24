2da7f0fe34f821a6035e94574c269012e8593a4d
shivang2405 <shivangagarwal2405@gmail.com>
2023-10-25 00:48:02 +0530

    added multiply.java

---
+++ multiply.java
@@ -0,0 +1,8 @@
+public class multiply{
+    public static void main(String[] args){
+        int a = 5;
+        int b = 10;
+        int c = a * b;
+        System.out.println("The product is " + c);
+    }
+}
